d493912062c689f6b8db45513c20f42731da5c5f
Aidul Azmi. S <aidulazmi@gmail.com>
2021-06-05 11:53:08 +0700

    gas

--- application/views/client/sl/home.php
+++ application/views/client/sl/home.php
@@ -39,6 +39,7 @@
                 <div class="col-lg-12">
                     <nav class="navbar navbar-expand-lg navbar-light">
                         <a class="navbar-brand" href="#"> <img src="<?=base_url();?>/asset/umum/img/uin3.png" alt="logo"> </a>
+                        <h5>PT. Servitama Internusa Pekanbaru</h5>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                             <span class="navbar-toggler-icon"></span>
                         </button>
